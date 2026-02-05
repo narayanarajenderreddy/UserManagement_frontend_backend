04ffc82f6fe2c5ba628b4c06948aec92df136e2c
rajenderreddy narayana <narayanarajenderreddy0913@gamil.com>
2026-02-05 13:37:39 +0530

    added RBAC for apis authorization

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app.api.deps import get_current_user
 from app.models.user import User
+from app.api.permission import require_role
+from app.core.roles import Roles
 from app.schemas.auth import CurrentUserResponse
 
 
@@ -9,7 +11,15 @@ router = APIRouter(prefix = "/users",tags=["Users"])
 
 @router.get("/me")
 def read_current_user(current_user:CurrentUserResponse=Depends(get_current_user)):
-    return current_user
     if not current_user:
         raise HTTPException(status_code=404, detail="User not found")
     return current_user
+
+@router.get("/admin-only")
+def admin_endpoint(
+    admin_user: User = Depends(require_role(Roles.ADMIN))
+):
+    return {
+        "message": "Welcome Admin",
+        "admin_id": admin_user.id
+    }
